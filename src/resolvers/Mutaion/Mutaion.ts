a62a45cd030f0f9234cc1a21523cc3986c8a1c25
johirul-islam-6 <johirulislam574206@gmail.com>
2023-11-16 22:53:18 +0600

    file sytem update

---
+++ src/resolvers/Mutaion/Mutaion.ts
@@ -0,0 +1,93 @@
+import config from "../../config"
+import { JwtHelpers } from "../../utils/jwtHelper"
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+
+
+const prisma = new PrismaClient()
+
+ interface userinfo {
+    name : string,
+    email : string,
+    password : string,
+    bio : string
+  }
+
+export const  Mutation={
+    signup: async (parent : any, args:userinfo, context:any) => {
+    
+      const isExist = await prisma.user.findFirst({
+        where : {
+          email : args.email
+        }
+      })
+
+      if(isExist){
+        return{
+          message : "This email already registered",
+          token : null
+        }
+      }
+
+      const hashPassword = await bcrypt.hash(args.password, 12)
+      const newUser = await prisma.user.create({
+        data : {
+          name : args.name,
+          email : args.email,
+          password : hashPassword,
+    
+        }
+      })
+      // ----- when bio write a user then create profile -----
+      if(args.bio){
+        await prisma.profile.create({
+          data : {
+            bio : args.bio,
+            userId : newUser.id
+          }
+        })
+      }
+
+      const token = JwtHelpers({userId : newUser.id},config.jwt.secret as string);
+      return {
+        token,
+        message : "user successfully login"
+      }
+    },
+    //  -----log in ----
+    signIn: async(parent : any , args : any, context:any) =>{
+       
+      const user = await prisma.user.findFirst({
+        where : {
+          email : args.email
+        }
+      });
+
+      if(!user){
+        return {
+          message : "email incorrect",
+           token : null
+         
+        }
+      }
+        // --- password checking --
+      const correctPassword = await bcrypt.compare(args.password, user.password)
+      if(!correctPassword){
+        return{
+          message : "password incorrect",
+           token : null
+        }
+      }
+
+      // ---- everything ok ----
+      // const token = jwt.sign({userId : user.id}, "signature", {expiresIn: "2d"});
+      const token = JwtHelpers({userId : user.id}, config.jwt.secret as string);
+
+      return {
+        token,
+        message : "user successfully login"
+      }
+    }
+
+  }
---
+++ src/resolvers/Query/Query.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const Query = {
+    users : async(parent : any, args:any, context:any) => {
+      return await prisma.user.findMany()
+    
+    }
+   
+  }
